89cc883ad2482a59d9cc6fa053c7a0d24c91b36f
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-07-27 16:08:18 +0200

    added test

--- Example/test/TestApp.js
+++ Example/test/TestApp.js
@@ -8,6 +8,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { styles, ItemSeparator, MainScreenItem } from '../src/App';
 
 import SimpleTest from './SimpleTest';
+import FastRefreshTest from './FastRefreshTest';
 
 YellowBox.ignoreWarnings(['Calling `getNode()`']);
 
@@ -16,23 +17,27 @@ const SCREENS = {
     screen: SimpleTest,
     title: '🆕 Simple test',
   },
+  FastRefreshTest: {
+    screen: FastRefreshTest,
+    title: '🆕 Fast refresh test',
+  },
 };
 
 function MainScreen({ navigation }) {
-  const data = Object.keys(SCREENS).map((key) => ({ key }));
+  const data = Object.keys(SCREENS).map(key => ({ key }));
   return (
     <FlatList
       style={styles.list}
       data={data}
       ItemSeparatorComponent={ItemSeparator}
-      renderItem={(props) => (
+      renderItem={props => (
         <MainScreenItem
           {...props}
           screens={SCREENS}
           onPressItem={({ key }) => navigation.navigate(key)}
         />
       )}
-      renderScrollComponent={(props) => <ScrollView {...props} />}
+      renderScrollComponent={props => <ScrollView {...props} />}
     />
   );
 }
@@ -57,8 +62,8 @@ const TestApp = createSwitchNavigator({
 });
 
 const createApp = Platform.select({
-  web: (input) => createBrowserApp(input, { history: 'hash' }),
-  default: (input) => createAppContainer(input),
+  web: input => createBrowserApp(input, { history: 'hash' }),
+  default: input => createAppContainer(input),
 });
 
 export default createApp(TestApp);
